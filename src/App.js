8c592e1226f43f893cecf33373e8d507eeb52be4
Vladyslav HAVRYLENKO <vhavryle@e1r4p1.unit.ua>
2019-01-03 17:48:51 +0200

    03jan

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-            go & fck yslf!
-          </a>
-        </header>
-          < div className ="jumbotron" > test </div>
+      <div className="container-fluid">
+            <h1>3 different blocks</h1>
+            <div className="row">
+                  <div className="col-md-4" style={{backgroundColor: "#fff999"}}>
+                      Left
+                  </div>
+                  <div className="col-md-4" style={{backgroundColor: "#fdf"}}>
+                      Center
+                  </div>
+                  <div className="col-md-4" style={{backgroundColor: "#777"}}>
+                      Right
+                  </div>
+            </div>
       </div>
 
     );

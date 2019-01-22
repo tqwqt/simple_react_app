0035d4c8f4a51d82c5865150fbeca5e650c916aa
Vladyslav HAVRYLENKO <vhavryle@e1r4p16.unit.ua>
2019-01-22 18:26:51 +0200

    new boots4

--- src/App.js
+++ src/App.js
@@ -10,20 +10,7 @@ class App extends Component {
     	<div>
 	        <NavbarComponent/>
 		    <div className="container">
-				<div className="dropdown">
-					<button className="btn btn-info dropdown-toggle" data-toggle="dropdown">
-						List<span className="caret"></span>
-					</button>
-					<ul className="dropdown-menu">
-						<li className="dropdown-header">Header 1</li>
-						<li className="disabled"><a href="#">Css</a></li>
-						<li><a href="#">HTML</a></li>
-						<li><a href="#">JQuery</a></li>
-						<li className="divider"></li>
-						<li className="dropdown-header">Header 2</li>
-						<li><a href="#">JS</a></li>
-					</ul>
-				</div>
+				
 		    </div>
 
 	    </div>
@@ -35,87 +22,3 @@ class App extends Component {
 }
 
 export default App;
-{/*<form role={"form"}>*/}
-	{/*<div className={"form-group"}>*/}
-		{/*<label className="control-label col-md-2" htmlFor="name">Имя</label>/!*control-label is needed for grid system*!/*/}
-		{/*<div className="col-md-10">*/}
-			{/*<input type={"text"} className={"form-control"} id={"name"} placeholder={"Введите имя"}/>*/}
-			{/*<p className="help-block">Some help</p>*/}
-		{/*</div>*/}
-	{/*</div>*/}
-	{/*<select className="form-control">*/}
-		{/*<option>1</option>*/}
-		{/*<option>3</option>*/}
-		{/*<option>2</option>*/}
-	{/*</select>*/}
-	{/*<div className="radio">*/}
-		{/*<label>*/}
-			{/*<input type="radio" checked/>*/}
-			{/*First*/}
-		{/*</label>*/}
-	{/*</div>*/}
-	{/*<div className="radio">*/}
-		{/*<label>*/}
-			{/*<input type="radio" />*/}
-			{/*Second*/}
-		{/*</label>*/}
-	{/*</div>*/}
-	{/*<div className={"form-group"}>*/}
-		{/*<label className="control-label col-md-2" htmlFor="surname">Фамилия</label>*/}
-		{/*<div className="col-md-10">*/}
-			{/*<input type={"text"} className={"form-control"} id={"surname"} placeholder={"Введите имя"}/>*/}
-			{/*<p className="help-block">Some help</p>*/}
-		{/*</div>*/}
-	{/*</div>*/}
-	{/*<div className="checkbox">*/}
-		{/*<label htmlFor="">*/}
-			{/*<input type="checkbox"/>CheckBox*/}
-		{/*</label>*/}
-	{/*</div>*/}
-	{/*<button type="button" className="btn btn-success">Отправить</button>*/}
-{/*</form>*/}
-//////////////////////////////
-{/*<Navbar>*/}
-{/*<Navbar.Header>*/}
-{/*<Navbar.Brand>*/}
-{/*<a href="#home">Brand</a>*/}
-{/*</Navbar.Brand>*/}
-{/*<Navbar.Toggle />*/}
-{/*</Navbar.Header>*/}
-{/*<Navbar.Collapse>*/}
-{/*<Navbar.Text>*/}
-{/*Signed in as: <Navbar.Link href="#">Mark Otto</Navbar.Link>*/}
-{/*</Navbar.Text>*/}
-{/*<Navbar.Text pullRight>Have a great day!</Navbar.Text>*/}
-{/*</Navbar.Collapse>*/}
-{/*</Navbar>*/}
-//--------------------------
-{/*<nav className="navbar navbar-expand-sm navbar-dark bg-dark ">*/}
-    {/*<div className={"container-fluid "}>*/}
-        {/*<div className={"navbar-header"}>*/}
-            {/*<a href="#" className={"navbar-brand "}>HackDesign</a>*/}
-        {/*</div>*/}
-        {/*<button className="navbar-toggler" type="button" data-toggle data-target="#collapsibleNavbar">*/}
-            {/*<span className="navbar-toggler-icon"></span>*/}
-        {/*</button>*/}
-        {/*<div className={"collapse navbar-collapse"} id={"collapsibleNavbar"}>*/}
-            {/*<ul className={"navbar-nav"}>*/}
-                {/*<li className="nav-item active"> <a className="nav-link" href="#">Главная</a></li>*/}
-                {/*<li className="nav-item"> <a className="nav-link" href="#">Обо мне</a></li>*/}
-                {/*<li className="nav-item"> <a className="nav-link" href="#">Обратная связь</a></li>*/}
-            {/*</ul>*/}
-        {/*</div>*/}
-    {/*</div>*/}
-{/*</nav>*/}
-//-------------------------
-{/*<div className="container-fluid">*/}
-{/*<div className="row">*/}
-{/*<div className="col-sm-12 order-sm-1 col-lg-5 order-lg-2" style={{backgroundColor: "#fff999"}}>*/}
-{/*<div className="row">*/}
-{/*<div className="col-lg-6" style={{backgroundColor: "#ff2345"}}>Uno</div>*/}
-{/*<div className="col-lg-6" style={{backgroundColor: "#ff2ee5"}}>Duos</div>*/}
-{/*</div>*/}
-{/*</div>*/}
-{/*<div className="col-sm-12 order-sm-2 col-lg-7 order-lg-1" style={{backgroundColor: "#ff6666"}}>Second</div>*/}
-{/*</div>*/}
-{/*</div>*/}
--- src/components/Header/NavbarComponent.js
+++ src/components/Header/NavbarComponent.js
@@ -5,25 +5,21 @@ import {Navbar} from 'react-bootstrap/lib'
 class NavbarComponent extends Component {
     render() {
         return (
-            <Navbar inverse fluid={"true"}>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#home">HackDesign</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Text>
-                        <Navbar.Link href="#">Главная</Navbar.Link>
-                    </Navbar.Text>
-                    <Navbar.Text>
-                        <Navbar.Link href="#">О себе</Navbar.Link>
-                    </Navbar.Text>
-                    <Navbar.Text>
-                        <Navbar.Link href="#" >Обратная связь</Navbar.Link>
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
+                <a className="navbar-brand" href="#">HackDesign</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                <li className="navbar-item active"><a className="nav-link" >Главная</a></li>
+                <li className="navbar-item "><a className="nav-link" >О себе</a></li>
+                <li className="navbar-item "><a className="nav-link" >Обратная связь</a></li>
+                </ul>
+                </div>
+            </nav>
         );
     }
 }
@@ -31,3 +27,26 @@ class NavbarComponent extends Component {
 NavbarComponent.propTypes = {};
 
 export default NavbarComponent;
+
+
+
+//-----------------------
+{/*<Navbar inverse fluid={"true"}>*/}
+    {/*<Navbar.Header>*/}
+        {/*<Navbar.Brand>*/}
+            {/*<a href="#home">HackDesign</a>*/}
+        {/*</Navbar.Brand>*/}
+        {/*<Navbar.Toggle />*/}
+    {/*</Navbar.Header>*/}
+    {/*<Navbar.Collapse>*/}
+        {/*<Navbar.Text>*/}
+            {/*<Navbar.Link href="#" >Главная</Navbar.Link>*/}
+        {/*</Navbar.Text>*/}
+        {/*<Navbar.Text>*/}
+            {/*<Navbar.Link href="#">О себе</Navbar.Link>*/}
+        {/*</Navbar.Text>*/}
+        {/*<Navbar.Text>*/}
+            {/*<Navbar.Link href="#" >Обратная связь</Navbar.Link>*/}
+        {/*</Navbar.Text>*/}
+    {/*</Navbar.Collapse>*/}
+{/*</Navbar>*/}
